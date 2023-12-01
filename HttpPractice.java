19230044c67a1e4ec8fea75fdcd6b585015a00fe
jonmbraverman <jon.m.braverman@gmail.com>
2023-12-01 08:27:02 -0500

    Using webhook site

--- HttpPractice.java
+++ HttpPractice.java
@@ -8,7 +8,7 @@ public class HttpPractice {
 
     public static void main(String[] args)
     {
-        String http = "https://httpbin.org/headers";
+        String http =  "https://webhook.site/2817d0a4-d418-45d1-8bcd-64890a4fdbf0"; //"https://httpbin.org/headers";
         //String http = "https://www.google.com";
         System.out.printf("Test url: %s\n\nresponse:\n", http);
         HttpClient client = HttpClient.newHttpClient();
