eb1bb0d92a70e63e3d20419ad8928ad182e8368c
jonmbraverman <jon.m.braverman@gmail.com>
2023-11-30 23:11:31 -0500

    Initial commit of basic example

---
+++ HttpPractice.java
@@ -0,0 +1,27 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class HttpPractice {
+
+    public static void main(String[] args)
+    {
+        //String http = "https://httpbin.org/headers";
+        String http = "http://www.google.com";
+        System.out.printf("Test: %s:\n", http);
+        HttpClient client = HttpClient.newHttpClient();
+        
+        HttpRequest request = HttpRequest.newBuilder()
+         .uri(URI.create(http))
+         .build();
+        
+         client.sendAsync(request, BodyHandlers.ofString())
+         .thenApply(HttpResponse::body)
+         .thenAccept(System.out::println)
+         .join(); 
+
+        
+    }
+}
